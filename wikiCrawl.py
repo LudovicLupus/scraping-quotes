e61ca95d22febf659545a6cb6e4c8ddbf23c90f8
LudovicLupus <LudovicLupus@gmail.com>
2020-02-07 16:57:37 -0500

    Small updates

--- wikiCrawl.py
+++ wikiCrawl.py
@@ -26,10 +26,14 @@ print(f"This is soup.a['id']: {soup.a['id']}")
 
 # Tags' children are available in a list called .contents
 print("Profiling object types:\n")
-print(f"THIS IS soup:\n {type(soup)}")                                                     # <class 'bs4.BeautifulSoup'>
+print(f"THIS IS soup:\n {type(soup)}")                                                      # <class 'bs4.BeautifulSoup'>
 print(f"THIS IS soup.body:\n {type(soup.body)}")                                            # <class 'bs4.element.Tag'>
 print(f"THIS IS soup.body.find_all('div')):\n {type(soup.body.find_all('div'))}")           # <class 'bs4.element.ResultSet'>
 print(f"THIS IS soup.body.find_all('div')[3]):\n {type(soup.body.find_all('div')[3])}")     # <class 'bs4.element.Tag'>
+
+print("\n")
+# PENDING: How to navigate tag tree
+
 
 # print(f"tag_p: {tag_p}")
 #
